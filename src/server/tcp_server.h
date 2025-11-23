ac3b3d1bccc0c90ba4ac4ac002723b87f0d7f9f4
sagomes <sagomes@tripadvisor.com>
2025-11-23 20:49:28 +0000

    fix: remove init_tcp_server from header file

--- src/server/tcp_server.h
+++ src/server/tcp_server.h
@@ -6,7 +6,6 @@
 #include <netdb.h>
 #include <pthread.h>
 
-void init_tcp_server(char *port, int &socket_fd, struct addrinfo &hints, struct addrinfo *&res);
 void* tcp_server_thread(void* arg);
 
 #endif
